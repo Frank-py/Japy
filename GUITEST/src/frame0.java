2b005e40c6b144ff5e855559434c6be2d4a7d1db
Frank-py <Lenj94002@gmail.com>
2022-01-19 23:11:40 +0100

    third commitgit add .git add .

--- GUITEST/src/frame0.java
+++ GUITEST/src/frame0.java
@@ -7,19 +7,19 @@ public class frame0 extends JFrame {
   // JFrame = a GUI window to add components to
   
   frame0(){
-  this.setTitle("Close me!!!");
-  //this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-  this.setResizable(false); 
+  this.setTitle("Messenger");
+  this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  //this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+  this.setResizable(true); 
   this.setSize(1920,1080); 
   this.setVisible(true); 
   this.setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
   //this.toFront();
   this.setAlwaysOnTop(true);
  // this.setUndecorated(true);
-  ImageIcon image = new ImageIcon("profile.png"); 
+  ImageIcon image = new ImageIcon("index.png"); 
   this.setIconImage(image.getImage()); 
-  this.getContentPane().setBackground(new Color(0x123456)); 
+  this.getContentPane().setBackground(new Color(0x000000)); 
     
  }
 }
--- GUITEST/src/run.java
+++ GUITEST/src/run.java
@@ -2,15 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 
 
-public class run {
-    
-    public static void main(String[] args)
-   {
-      // int x = 1;
-     //  while (x>=0 ){
-    frame0 test = new frame0();
-    System.out.println("hi");
- // x++;
- //  }
+public class run {    
+   public static void main(String[] args) {
+      frame0 test = new frame0();
    }
 }
