7c4955f044458981d65d44c97e8fb4b258d46ce5
Frank-py <lenj94002@gmail.com>
2022-01-22 00:01:26 +0100

    next commit

--- src/Python/test/Host.py
+++ src/Python/test/Host.py
@@ -6,7 +6,11 @@ def getsock():
         s.listen()
         conn, addr = s.accept()
         data = conn.recv(1024).decode(encoding="UTF-8")
+        s.connect(("", 5000))
+        if data[0] == "login":
+            s.send(str(data))
+        elif data[0] == "Nachricht":
+            s.send(str(data))
         return data
 while True:
-    print(getsock())        
- 
+    print(getsock())
