3cd3d51631af26d29a10d5ca8f54602aefac4c53
Frank-py <lenj94002@gmail.com>
2022-01-22 00:01:26 +0100

    next commit

--- src/Python/Server.py
+++ src/Python/Server.py
@@ -1,1 +1,12 @@
 import socket
+from functionsserver import *
+def getsock():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(("", 5000))
+        s.listen()
+        conn, addr = s.accept()
+        data = conn.recv(1024).decode(encoding="UTF-8")
+        
+while True:
+    print(getsock())
+#connection = create_connection("ip", "username", "passwort", "database")
