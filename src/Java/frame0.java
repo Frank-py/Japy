2ed008706fdcf2c6f4c83a7d54c7e8db028534ef
Frank-py <lenj94002@gmail.com>
2022-01-23 13:57:28 +0100

    addding framelabelpanel0.java back

---
+++ src/Java/frame0.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class frame0 extends JFrame {
+
+
+  // JFrame = a GUI window to add components to
+  
+  frame0(){
+  this.setTitle("Close me!!!");
+  this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  //this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+  this.setResizable(true); 
+  this.setSize(1920,1080); 
+  this.setVisible(true); 
+  // this.setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
+  //this.toFront();
+  this.setAlwaysOnTop(false);
+ // this.setUndecorated(true);
+  ImageIcon image = new ImageIcon("prof.png"); 
+  this.setIconImage(image.getImage()); 
+  this.getContentPane().setBackground(new Color(0x123456)); 
+    
+ }
+}
