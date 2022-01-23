bd37ae8e794cb58c4d0b2009c486daa39dd476e6
wertzuz <quint.schartel@gmail.com>
2022-01-23 11:36:49 +0100

    Now using IntelliJ to code and have got a nice git plugin

--- src/Java/run.java
+++ src/Java/run.java
@@ -7,15 +7,14 @@ public class run {
    {
     frame0 mainWindow = new frame0();
     mainWindow.setTitle("NEVER");
-    
-    //label0 label = new label0();
-   // label.setText("Completed");
-   // panel0 panel = new panel0();
-   // mainWindow.add(panel);
 
-    //panel.add(label);
-    //mainWindow.add(label);
-		//.frame.pack();
-  // javaclient.send("hi");
+    label0 label = new label0();
+    label.setText("Completed");
+    panel0 panel = new panel0();
+    mainWindow.add(panel);
+   // panel.add(label);
+ // mainWindow.add(label);
+   // mainWindow.pack();
+   //javaclient.send("hi");
    }
 }
