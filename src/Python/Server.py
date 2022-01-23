eb3c439874facb2481adc6079f1b9035623835fc
Frank-py <lenj94002@gmail.com>
2022-01-23 13:31:57 +0100

    funciiotns pyhton

--- src/Python/Server.py
+++ src/Python/Server.py
@@ -6,6 +6,22 @@ def getsock():
         s.listen()
         conn, addr = s.accept()
         data = conn.recv(1024).decode(encoding="UTF-8")
+        data = data.split(" ")
+        if data[0] == "register":
+            connection = create_connection("localhost", "daniel", "4sdf38§$/WE3/FW§459fd2w3", "Messenger")
+            register(connection, data[1:])
+        elif data[0] == "login":
+            connection = create_connection("localhost", "daniel", "4sdf38§$/WE3/FW§459fd2w3", "Messenger")
+            login(connection, data[1:])
+        elif data[0] == "updateip":
+            connection = create_connection("localhost", "daniel", "4sdf38§$/WE3/FW§459fd2w3", "Messenger")
+            updateip(connection, data[1:])
+        elif data[0] == "insertmessage":
+            connection = create_connection("localhost", "daniel", "4sdf38§$/WE3/FW§459fd2w3", "Messenger")
+            insertmessage(connection, data[1:])
+        elif data[0] == "fetchmessages":
+            connection = create_connection("localhost", "daniel", "4sdf38§$/WE3/FW§459fd2w3", "Messenger")
+            checkformessages(connection, data[1:])
         
 while True:
     print(getsock())
