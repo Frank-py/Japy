9e428e24f9115ba39e985149ff3ec2686a050a19
wertzuz <quint.schartel@gmail.com>
2022-01-23 13:11:08 +0100

    new try

--- src/Java/label0.java
+++ src/Java/label0.java
@@ -3,26 +3,24 @@
  */
 import java.awt.Color;
 import java.awt.Font;
-import javax.swing.*;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.BorderFactory;
 import javax.swing.border.Border;
+
 public class label0 extends JLabel {
-
-
 	label0() {
-
 		// JLabel = a GUI display area for a string of text, an image, or both
-		
 		ImageIcon image = new ImageIcon("prof.png");
-		Border border = BorderFactory.createLineBorder(Color.green,3);
-		
-		this.setText("Messenger is loading"); //set text of label
 		this.setIcon(image);
+		Border border = BorderFactory.createLineBorder(Color.yellow,3);
+		this.setText("Messenger is loading"); //set text of label
 		this.setHorizontalTextPosition(JLabel.CENTER); //set text LEFT,CENTER, RIGHT of imageicon
 		this.setVerticalTextPosition(JLabel.TOP); //set text TOP,CENTER, BOTTOM of imageicon
 		this.setForeground(new Color(0x00FF00)); //set font color of text
 		this.setFont(new Font("MV Boli",Font.PLAIN,10)); //set font of text
 		this.setIconTextGap(100); //set gap of text to image
-		this.setBackground(Color.black); //set background color
+		this.setBackground(Color.green); //set background color
 		this.setOpaque(true); //display background color
 		this.setBorder(border); //sets border of label (not image+text)
 		this.setVerticalAlignment(JLabel.CENTER); //set vertical position of icon+text within label
