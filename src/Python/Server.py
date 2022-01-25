2bf791ff4e39c425ecc768d6bc9a8f47ab1eae26
Frank-py <lenj94002@gmail.com>
2022-01-25 22:01:42 +0100

    python

--- src/Python/Server.py
+++ src/Python/Server.py
@@ -1,30 +1,19 @@
-import socket, re
+import socket, re, threading
 from functionsserver import *
-def getsock():
-    connection = create_connection("localhost", "daniel", "4sdf38§$/WE3/FW§459fd2w3", "Messenger")
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(("", 5000))
-        s.listen()
+PORT = 5000
+
+def Client(conn, addr):
+    data = conn.recv(1024).decode(encoding="UTF-8")
+    data = data.split(" ")
+    if data[0] == "login":
+        Benutzer = User().checkaccount(data[1], data[2], addr)
+    
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("", PORT))
+    s.listen()
+    while True:
         conn, addr = s.accept()
-        data = conn.recv(1024).decode(encoding="UTF-8")
-        data = data.split(" ")
-        if data[0] == "register":
-            register(connection, data[1:])
-        elif data[0] == "login":
-            login(connection, data[1:])
-        elif data[0] == "updateip":
-            updateip(connection, data[1:])
-        elif data[0] == "insertmessage":
-            insertmessage(connection, data[1:])
-        elif data[0] == "checkformessages":
-            a = re.sub(r'[^A-Za-z0-9_]', '', data[1])
-            return checkformessages(connection, data[1].strip())
-        elif data[0] == "checkaccount":
-            if checkaccount(connection, data[1]):
-                login(connection, data[1:])
-            else:
-                register(connection, data[1:])
-        else:
-            return checkformessages(connection, data[1])
-while True:
-    print(getsock())
+        thread = threading.Thread(target = Client, args=(conn, addr))
+        thread.start()
+        print(f"Verbindungen: {threading.active_count() - 1}") #wegen main()
