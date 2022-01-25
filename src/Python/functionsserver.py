e4d82a940fd04ccff2dfff372aa5ac385f79d622
Frank-py <lenj94002@gmail.com>
2022-01-25 22:01:42 +0100

    python

--- src/Python/functionsserver.py
+++ src/Python/functionsserver.py
@@ -1,81 +1,93 @@
-from sqlite3 import Cursor
-from turtle import up
 import mysql.connector
 
 from mysql.connector import Error
 
-
-def create_connection(host_name, user_name, user_password, db_name):
-    connection = None
-    try:
-        connection = mysql.connector.connect(
-            host=host_name,
-            user=user_name,
-            passwd=user_password,
-            database=db_name
+class User():
+    def __init__(self, user=None, password=None, ip=None, loggedin=None):
+        self.user = user
+        self.password = password
+        self.ip = ip
+        self.connection = self.create_connection()
+        self.loggedin = loggedin
+    def create_connection(self):
+        connection = None
+        try:
+            connection = mysql.connector.connect(
+                 host="localhost",
+                  user="***REMOVED***",
+                  passwd="***REMOVED***",
+                  database="***REMOVED***"
         )
-        print("Connection to MySQL DB successful")
-    except Error as e:
-        print(f"The error '{e}' occurred")
-    return connection
-def create_database(connection, query):
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query)
-        print("Database created successfully")
-    except Error as e:
-        print(f"The error '{e}' occurred")
-def login(connection, user):
-    cursor = connection.cursor()
-    try:
-        cursor.execute('SELECT Password from Persons where Username = "%s";' % (user))
-        myresult = cursor.fetchone()[0]
-        return myresult
-    except Error as e:
-        print(e)
-def register(connection, liste):
-    cursor = connection.cursor()
-    try:
-        sql = 'INSERT INTO Persons (Username, Password, IP) VALUES (%s, %s, %s);'
-        val = (liste[0], liste[1], liste[2].strip())
-        cursor.execute(sql, val)
-        connection.commit()
-    except Error as e:
-        print(e)
-def updateip(connection, liste):
-    cursor = connection.cursor()
-    try:
-        cursor.execute("UPDATE Persons SET IP = '%s' WHERE Username = '%s';" % (liste[0], liste[1]))
-        connection.commit()
-    except Error as e:
-        print(e)
-def insertmessage(connection, liste):
-    cursor = connection.cursor()
-    try:
-        sql = "INSERT INTO Messages (send, recv, message) VALUES (%s, %s, %s);"
-        val = (liste[0], liste[1], " ".join(liste[2:]).strip())
-        cursor.execute(sql, val)
-        connection.commit()
-    except:
-        print("error")
-def checkformessages(connection, receiver):
-    cursor = connection.cursor()
-    try:
-        cursor.execute('SELECT message FROM Messages WHERE recv = "%s";' % (receiver))
-        nachrichten = cursor.fetchall()
-        listen = []
-        for i in nachrichten:
-            listen.append(i[0])
-        return listen
-    except:
-        return "error"
-def checkaccount(connection, name):
-    cursor = connection.cursor()
-    try:
-        cursor.execute('SELECT 1 FROM Persons WHERE Username = "%s";' % (name))
-        nachrichten = cursor.fetchone()[0]
-        return name == name
-    except:
-        return False
-           
-print(checkaccount(create_connection("localhost", "daniel", "4sdf38§$/WE3/FW§459fd2w3", "Messenger"), "Valentina"))
+        except Error as e:
+            print(f"The error '{e}' occurred")
+        return connection
+    def create_database(self, query):
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute(query)
+            print("Database created successfully")
+        except Error as e:
+            print(f"The error '{e}' occurred")
+    @classmethod
+    def login(cls, connection, user, password, ip):
+        cursor = connection.cursor()
+        try:
+            cursor.execute('SELECT Password from Persons where Username = "%s";' % (user))
+            myresult = cursor.fetchone()[0]
+            if myresult == password:
+                return cls(user, password, ip, True)
+            else:
+                return "wrong password"
+        except Error as e:
+            print(e)
+    @classmethod
+    def register(cls, connection, username, password, ip):
+        cursor = connection.cursor()
+        try:
+            sql = 'INSERT INTO Persons (Username, Password, IP) VALUES (%s, %s, %s);'
+            val = (username, password, ip.strip())
+            cursor.execute(sql, val)
+            connection.commit()
+            return cls(username, password, ip.strip(), True)
+        except Error as e:
+            return "error"
+    def updateip(self, connection, username, ip):
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("UPDATE Persons SET IP = '%s' WHERE Username = '%s';" % (ip, username))
+            connection.commit()
+            self.ip = ip
+        except Error as e:
+            print(e)
+    def insertmessage(self, liste):
+        cursor = self.connection.cursor()
+        try:
+            sql = "INSERT INTO Messages (send, recv, message) VALUES (%s, %s, %s);"
+            val = (self.user, self.recv, " ".join(liste[2:]).strip())
+            cursor.execute(sql, val)
+            self.connection.commit()
+        except:
+            print("error")
+    def checkformessages(self, receiver):
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute('SELECT message FROM Messages WHERE recv = "%s" AND send = "%s";' % (receiver, self.user))
+            nachrichten = cursor.fetchall()
+            listen = []
+            for i in nachrichten:
+                listen.append(i[0])
+            return listen
+        except:
+            return "error"
+    def checkaccount(self, name, password, ip):
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute('SELECT 1 FROM Persons WHERE Username = "%s";' % (name))
+            nachrichten = cursor.fetchone()[0]
+            if name == name:
+                return self.login(self.connection, name, password, ip)
+        except:
+            return self.register(self.connection, name, password, ip)
+
+
+
