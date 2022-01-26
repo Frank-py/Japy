17fac6a0e6d17110ea8be61d11d4cbb80e582d88
wertzuz <quint.schartel@gmail.com>
2022-01-27 00:39:01 +0100

    hä

---
+++ src/Java/sendrecv.java
@@ -0,0 +1,21 @@
+package Java;
+import java.io.*;
+import java.net.Socket;
+//import java.net.UnknownHostException;
+public class sendrecv {
+    public static String send(String a, Socket socket) /*throws UnknownHostException, ClassNotFoundException, InterruptedException*/{
+        try {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out.print(a);
+            out.flush();
+            String resp = in.readLine();
+            socket.close();
+            return resp;          
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "error";
+        }
+        
+    }
+}
