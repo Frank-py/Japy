3c2034934d71d48fd01d464105697f80652a6300
Frank-py <lenj94002@gmail.com>
2022-01-28 16:43:22 +0100

    productive

--- src/Java/sendrecv.java
+++ src/Java/sendrecv.java
@@ -1,33 +1,28 @@
-package Japy.src.Java;
+package Java;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.stream.Stream;
 
 //import java.net.UnknownHostException;
 public class sendrecv {
     //public static Stream<String> resp = null;
+    public static String s = null;
     public static String send(Socket socket, String Funktion, String args[]) { 
         try {
             if (Funktion == "login") {
                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 out.print("login");
                 out.flush();
                 out.print(args[0].replaceAll("\r", "").replaceAll("\n", ""));
                 out.flush();
                 out.print(args[1].replaceAll("\r", "").replaceAll("\n", ""));
                 out.flush();
-                //System.out.println(in.ready());
-                //resp = in.lines();
+                InputStreamReader in = new InputStreamReader(socket.getInputStream());
+                BufferedReader bf = new BufferedReader(in);
+                s = bf.readLine();
+                System.out.println(s);
 
-        
-                String resp;
-                /* while ((resp = in.readLine()) != null) {
-                if ("$".equals(resp)) {
-                    break;
-                } */
-        }
+                }
 
            // }
 
@@ -35,7 +30,7 @@ public class sendrecv {
             e.printStackTrace();
             return "error";
         }
-        return "1";
+        return s;
 
     }
 }
--- src/Python/Server.py
+++ src/Python/Server.py
@@ -1,34 +1,25 @@
 import socket, re, threading, hashlib
-#from functionsserver import *
+from functionsserver import *
 PORT = 5000
 
 def Client(conn, addr):
     while True:
         data = conn.recv(1024).decode(encoding="UTF-8")
-        print(data)
-        data = conn.recv(1024).decode(encoding="UTF-8")
-        println(data)
-        data = conn.recv(1024).decode(encoding="UTF-8")
-        print(data)
-        conn.send("richtiges Passwort$".encode('utf-8'))
-        # if data == "login":
-        #     data = []
-        #     data.append(conn.recv(1024).decode(encoding="UTF-8"))
-        #     data.append(conn.recv(1024).decode(encoding="UTF-8"))
-        #     print(data)
-        #     benutzer = User().checkaccount(data[0], hashlib.md5(bytes(data[1], encoding='UTF-8')).hexdigest(), addr[0])
-        #     if benutzer.loggedin and benutzer.registriert:
-        #         print("registriert")
-        #         conn.send("registriert".encode('utf-8'))
-        #     elif benutzer.loggedin:
-        #         print("richtiges Passwort")
-        #         conn.send("richtiges Passwort".encode('utf-8'))
-        #     else:
-        #         print("falsches Passwort")
-        #         conn.send("falsches Passwort".encode('utf-8'))
-        # else:
-        #     pass
-        #     conn.close()
+        if data == "login":
+            data = []
+            data.append(conn.recv(1024).decode(encoding="UTF-8"))
+            data.append(conn.recv(1024).decode(encoding="UTF-8"))
+            print(data)
+            benutzer = User().checkaccount(data[0], hashlib.md5(bytes(data[1], encoding='UTF-8')).hexdigest(), addr[0])
+            if benutzer.loggedin and benutzer.registriert:
+                conn.send("registriert\n".encode('utf-8'))
+            elif benutzer.loggedin:
+                conn.send("richtiges Passwort\n".encode('utf-8'))
+            else:
+                conn.send("falsches Passwort\n".encode('utf-8'))
+        else:
+            pass
+            conn.close()
     
     
 def main():
