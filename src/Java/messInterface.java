3baf72c251a1c0ac884e6249fe932976aee1702e
wertzuz <quint.schartel@gmail.com>
2022-01-29 22:49:55 +0100

    added checkboxes and fix bug

--- src/Java/messInterface.java
+++ src/Java/messInterface.java
@@ -1,8 +1,10 @@
 package Java;
 
+import javax.swing.*;
 
 public class messInterface {
 public static void createGUI(int log) {
+    JFrame frame = new JFrame();
     System.out.println(log);
     
 }
