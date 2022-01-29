06f222cefc97f2d16812fdfa7441628ee2a06eb2
wertzuz <quint.schartel@gmail.com>
2022-01-29 10:28:36 +0100

    new filestructure

---
+++ src/Java/examplecodes/panel0.java
@@ -0,0 +1,14 @@
+package Java.examplecodes;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.*;
+
+public class panel0 extends JPanel {
+
+    panel0() {
+        // JPanel = a GUI component that functions as a container to hold other components
+        this.setBackground(Color.red);
+        this.setBounds(100, 100, 100, 100);
+        this.setLayout(new BorderLayout());
+    }
+}
