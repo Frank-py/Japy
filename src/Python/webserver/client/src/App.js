2176eb35027985a10bb0cd7d3364206cdb6333e2
Frank-py <lenj94002@gmail.com>
2022-02-07 20:00:18 +0100

    asfd

--- src/Python/webserver/client/src/App.js
+++ src/Python/webserver/client/src/App.js
@@ -1,8 +1,11 @@
 import React, {useState, useEffect} from 'react';
+import "./style.css"
 function App() {
-  return (<div>
-    
-    </div>)
+  return (<body><div class="eingabe">
+    <p><input placeholder='Username'></input></p>
+    <input placeholder='Password'></input>
+    <p><button class="button">Login</button></p>
+    </div></body>)
 }
 
 export default App;
