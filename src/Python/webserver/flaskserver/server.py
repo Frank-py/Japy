d4311a81c541683d03f42b71146225e6eb09a5af
Frank-py <lenj94002@gmail.com>
2022-02-07 21:16:10 +0100

    sdf

--- src/Python/webserver/flaskserver/server.py
+++ src/Python/webserver/flaskserver/server.py
@@ -1,5 +1,5 @@
 from flask import Flask
-import os, sys
+import os, sys, random
 sys.path.insert(1, os.path.join(sys.path[0], '../..'))
 from functionsserver import *
 Benutzer = User()
@@ -7,8 +7,10 @@ app = Flask(__name__)
 
 @app.route("/login")
 def login():
-     return {"loggedin": ["yes"]}
-
+     if 1 == random.randint(0,10):
+          return {"loggedin": ["yes"]}
+     else:
+          return {"loggedin": ["no"]}
 
 if __name__ == "__main__":
     app.run(debug=True)
