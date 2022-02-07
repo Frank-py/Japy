1afd1c0a7e08c9a276370291cd21ee6aab24f6c6
Frank-py <lenj94002@gmail.com>
2022-02-07 21:14:36 +0100

    akldfjöaslkdfjöljlkasdöjf

--- src/Python/webserver/client/src/App.js
+++ src/Python/webserver/client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState, useEffect} from 'react';
+import "./style.css"
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState([{}]);
+  useEffect(() => {
+    fetch("/login").then(
+      res => res.json()
+      ).then(
+        data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
+  return (<div className="eingabe">
+    <p><input placeholder='Username'></input></p>
+    <input placeholder='Password'></input>
+    <p><button className="button">{data["loggedin"]}</button></p>
+    </div>)
 }
 
 export default App;
