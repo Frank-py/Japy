ea0b85c7cad8d50e5ed5423f091035827890848e
CulaFreek <vali.endler@gmail.com>
2022-02-07 15:43:40 +0100

    hi

--- src/Java/run.java
+++ src/Java/run.java
@@ -1,9 +1,11 @@
 package Japy.src.Java;
+
 import java.net.*;
 import java.io.*;
 
 public class run {
   public static Socket socket;
+
   public static void main(String[] args) {
     try {
       socket = new Socket("localhost", 5000);
@@ -12,12 +14,9 @@ public class run {
     } catch (IOException e2) {
       e2.printStackTrace();
     }
-    
     int resp = login.createGUI(socket);
-    if (resp!=0) {
+    if (resp != 0) {
       System.out.println(resp);
     }
-    
   }
-  
 }
