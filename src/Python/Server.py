8245e09463a050df6acef157d545b0db0897902b
Frank-py <lenj94002@gmail.com>
2022-02-07 16:20:59 +0100

    asdf

--- src/Python/Server.py
+++ src/Python/Server.py
@@ -1,38 +1,31 @@
-import socket, re, threading, hashlib
-#from functionsserver import *
+import socket, threading, hashlib
+from functionsserver import *
 PORT = 5000
     
 def Client(conn, addr):
     while True:
-        #try:
-        #     data = conn.recv(1024).decode(encoding="UTF-8")
-        #    # print(data = conn.recv(1024).decode(encoding="UTF-8"))
-        #     data = conn.recv(1024).decode(encoding="UTF-8")
-        #    # print(data = conn.recv(1024).decode(encoding="UTF-8"))
+        data = []
+        try:
             data = conn.recv(1024).decode(encoding="UTF-8")
-        #   #  print(data = conn.recv(1024).decode(encoding="UTF-8"))
-        #     conn.send("5\n".encode('utf-8'))
+            print(data = conn.recv(1024).decode(encoding="UTF-8"))
+            data = conn.recv(1024).decode(encoding="UTF-8")
+            print(data = conn.recv(1024).decode(encoding="UTF-8"))
+            data = conn.recv(1024).decode(encoding="UTF-8")
+            print(data = conn.recv(1024).decode(encoding="UTF-8"))
             if data == "login":
-                try:
-                    data = []
-                    data.append(conn.recv(1024).decode(encoding="UTF-8"))
-                    data.append(conn.recv(1024).decode(encoding="UTF-8"))
-                    print(data)
+                benutzer = User().checkaccount(data[0], hashlib.md5(bytes(data[1], encoding='UTF-8')).hexdigest(), addr[0])
+                if benutzer.loggedin and benutzer.registriert:
                     conn.send("0\n".encode('utf-8'))
-                except Exception:
-                        conn.send("4\n".encode('utf-8'))
-                #benutzer = User().checkaccount(data[0], hashlib.md5(bytes(data[1], encoding='UTF-8')).hexdigest(), addr[0])
-               # if benutzer.loggedin and benutzer.registriert:
-                #    conn.send("0\n".encode('utf-8'))
-               # elif benutzer.loggedin:
-               #     conn.send("1\n".encode('utf-8'))
-               # else:
-               #     conn.send("2\n".encode('utf-8'))
+                elif benutzer.loggedin:
+                    conn.send("1\n".encode('utf-8'))
+                else:
+                    conn.send("2\n".encode('utf-8'))
             else:
                 conn.close()
-        # except OSError:
-        #     conn.close()
-        #     return
+        except OSError:
+            conn.send("4\n".encode('utf-8'))
+            conn.close()
+            return
     
 def main():
     while True:
@@ -44,4 +37,5 @@ def main():
         thread.start()
         print(f"Verbindungen: {threading.active_count() - 1}") #wegen main()
         thread.join()
-main()
+if __name__ == '__main__':
+    main()
