bbb2db384c59b4dd401e229a517fd017b3d4ac98
wertzuz <quint.schartel@gmail.com>
2022-02-20 17:25:35 +0100

    structural changes

--- src/Java/login.java
+++ src/Java/login.java
@@ -3,16 +3,30 @@ package Java;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class login {
+public class login implements ActionListener,KeyListener  {
   String recv = "500";
- 
   int counter = 0;
+  ImageIcon pic = new ImageIcon("Java/prof5.png");
+  JFrame frame = new JFrame();
+  Color color = new Color(27,37,43);
+  JLabel laby = new JLabel("Hier Anmelden:");
+  JPanel title = new JPanel();
+  JPanel UserInput = new JPanel(new GridLayout(8, 1));
+  JPanel button = new JPanel();
+  JPanel space = new JPanel();
+  JTextField user = new JTextField("");
+  JLabel UserTitle = new JLabel("Username: ");
+  JPasswordField pass = new JPasswordField();
+  JLabel PassTitle = new JLabel("Password: ");
+  JButton loginbu = new JButton("Login!");
+  JCheckBox robo = new JCheckBox("I'm not a robot");
+  JCheckBox EULA = new JCheckBox("Accept the EULA and our AGB");
+  JCheckBox news = new JCheckBox("Subscribe to the Newsletter", true);
   login(){
-    Color color = new Color(27,37,43);
+    
     //ImageIcon pic = new ImageIcon("Japy\\src\\Java\\prof.png");
     //ImageIcon pic = new ImageIcon("/home/daniel/Projekt/src/Java/prof.png");
-    ImageIcon pic = new ImageIcon("Java/prof5.png");
-    JFrame frame = new JFrame();
+    
     frame.setSize(320, 440);
     frame.setLocationRelativeTo(null);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,82 +35,60 @@ public class login {
     frame.setIconImage(pic.getImage());
     frame.getContentPane().setBackground(color);
 
-    JLabel laby = new JLabel("Hier Anmelden:");
+    
     laby.setFont(new Font("MV Boli", Font.BOLD, 30));
     laby.setForeground(Color.white);
 
-    JPanel title = new JPanel();
+    
     title.setBackground(color);
 
-    JPanel UserInput = new JPanel(new GridLayout(8, 1));
+    
     UserInput.setBackground(color);
 
-    JPanel button = new JPanel();
+    
     button.setBackground(color);
 
-    JPanel space = new JPanel();
+    
     space.setSize(50, 50);
     space.setVisible(false);
 
-    JTextField user = new JTextField("");
-    JLabel UserTitle = new JLabel("Username: ");
+    
     UserTitle.setFont(new Font("MV Boli", Font.BOLD, 20));
     UserTitle.setForeground(Color.white);
 
-    JPasswordField pass = new JPasswordField();
-    JLabel PassTitle = new JLabel("Password: ");
+   
     PassTitle.setFont(new Font("MV Boli", Font.BOLD, 20));
     PassTitle.setForeground(Color.white);
 
-    JButton loginbu = new JButton("Login!");
+    
     loginbu.setFont(new Font("MV Boli", Font.BOLD, 14));
     loginbu.setForeground(Color.white);
     loginbu.setBackground(new Color(47,49,54));
     loginbu.setFocusable(false);
     loginbu.setSize(200, 40);
 
-    JCheckBox robo = new JCheckBox("I'm not a robot");
+    
     robo.setFocusable(false);
     robo.setBackground(color);
     robo.setForeground(Color.white);
-
-    JCheckBox EULA = new JCheckBox("Accept the EULA and our AGB");
+   
     EULA.setFocusable(false);
     EULA.setBackground(color);
     EULA.setForeground(Color.white);
 
-    JCheckBox news = new JCheckBox("Subscribe to the Newsletter", true);
+   
     news.setFocusable(false);
     news.setBackground(color);
     news.setForeground(Color.white);
 
     KeyListener g = new KeyListener() {
       public void keyPressed(KeyEvent e) {
-        
-        if (e.getKeyChar() == KeyEvent.VK_ENTER) {
-         
+         if (e.getKeyChar() == KeyEvent.VK_ENTER) {
           if (String.valueOf(pass.getPassword()).equals("") | user.getText().equals("")) {
             recv = "2";
           } else if (EULA.isSelected() == false | robo.isSelected() == false) {
             recv = "3";
-            /*JProgressBar lullol = new JProgressBar(0);
-            lullol.setBounds(0, 0, 300, 420);
-            lullol.setStringPainted(true);
-            UserInput.add(lullol);
-            loginbu.setEnabled(false);
-            while (counter <= 100) {
-              lullol.setValue(counter);
-              try {
-                Thread.sleep(100);
-              } catch (Exception i) {
-                System.out.println("sldöjkhkvhsjkhheshjdfsjljflsajfjsaf" + i);
-              }
-              counter++;
-              System.out.println(counter);
-            }
-            loginbu.setEnabled(true);*/
-          }
-          else if (user.getText().length() > 20 | String.valueOf(pass.getPassword()).length() > 50){ 
+          } else if (user.getText().length() > 20 | String.valueOf(pass.getPassword()).length() > 50){ 
               recv = "4";
             } else {
             try {
@@ -173,6 +165,22 @@ public class login {
       } else if (Integer.parseInt(recv) == 3) {
             JOptionPane.showMessageDialog(null, "Proof Humanity and Accept EULA", "Accept required Terms",
             JOptionPane.ERROR_MESSAGE);
+             /*JProgressBar lullol = new JProgressBar(0);
+            lullol.setBounds(0, 0, 300, 420);
+            lullol.setStringPainted(true);
+            UserInput.add(lullol);
+            loginbu.setEnabled(false);
+            while (counter <= 100) {
+              lullol.setValue(counter);
+              try {
+                Thread.sleep(100);
+              } catch (Exception i) {
+                System.out.println("sldöjkhkvhsjkhheshjdfsjljflsajfjsaf" + i);
+              }
+              counter++;
+              System.out.println(counter);
+            }
+            loginbu.setEnabled(true);*/
             recv = "500";
       } else if (Integer.parseInt(recv) == 4){
           EULA.setSelected(false);
@@ -192,6 +200,26 @@ public class login {
       }
     }
   }
+  void enter(){
+    System.out.println("www");
+  }
+  @Override
+  public void actionPerformed(ActionEvent e) {
   
-
+  }
+  @Override
+  public void keyPressed(KeyEvent e) {
+    // TODO Auto-generated method stub
+    
+  }
+  @Override
+  public void keyReleased(KeyEvent e) {
+    // TODO Auto-generated method stub
+    
+  }
+  @Override
+  public void keyTyped(KeyEvent e) {
+    // TODO Auto-generated method stub
+    
+  }
 }
