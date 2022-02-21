32546d68db79c5a938c88de5afc8c1d3e3e957a8
CulaFreek <vali.endler@gmail.com>
2022-02-21 14:32:31 +0100

    udsaud

--- src/Java/run.java
+++ src/Java/run.java
@@ -4,20 +4,21 @@ package Java;
 public class run {
   public static void main(String[] args) {
     sendrecv.socket();
-    login Login =new login(); 
-    while (true){
-    if (Login.recv.equals("0") |Login.recv.equals("1")  ) {
-      new messInterface(0); 
-      System.out.println("er");
-    }}
-    
-    
-    //new messInterface(0);
-    /* int log = login.createGUI();
-    if (log == 4) {
-      return;
+    login Login = new login();
+    while (true) {
+      if (Login.recv.equals("0") | Login.recv.equals("1")) {
+        new messInterface(0);
+        System.out.println("er");
+      }
     }
-     messInterface.createGUI(0); 
-    */
+
+    // new messInterface(0);
+    /*
+     * int log = login.createGUI();
+     * if (log == 4) {
+     * return;
+     * }
+     * messInterface.createGUI(0);
+     */
   }
 }
