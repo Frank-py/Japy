bfc588b043d333121412abbca00ec1612eff3cc3
Frank-py <lenj94002@gmail.com>
2022-02-21 16:28:31 +0100

    tesst

--- src/Java/run.java
+++ src/Java/run.java
@@ -4,27 +4,12 @@ package Java;
 public class run {
   public static void main(String[] args) {
     sendrecv.socket();
-<<<<<<< HEAD
-    //new messInterface(0); 
-    login Login =new login(); 
-   /*  while (true){
-    if (Login.recv.equals("0") |Login.recv.equals("1")  ) {
-      System.out.println("er");
-    }} */
-    
-    
-    //new messInterface(0);
-    /* int log = login.createGUI();
-    if (log == 4) {
-      return;
-=======
     login Login = new login();
     while (true) {
       if (Login.recv.equals("0") | Login.recv.equals("1")) {
         new messInterface(0);
         System.out.println("er");
       }
->>>>>>> aaf577dd42c95ba58d9c74b4c51f148544be5cc1
     }
 
     // new messInterface(0);
