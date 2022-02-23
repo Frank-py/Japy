7d7b1e19c5dd64e90ca2dbe107831f7938adf620
wertzuz <quint.schartel@gmail.com>
2022-02-23 12:51:56 +0100

    ww

--- src/Java/run.java
+++ src/Java/run.java
@@ -5,10 +5,9 @@ public class run {
   public static void main(String[] args) {
     sendrecv.socket();
     //new messInterface(0); 
-    login Login = new login();
+     new login();
    /*  while (true){
     if (Login.recv.equals("0") |Login.recv.equals("1")  ) {
-      System.out.println("er");
     }} */
     
     
