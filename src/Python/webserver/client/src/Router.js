f4f94c8ca2e2a22765b75a00754fea7ed21bb9d1
Daniel <lenj94002@gmail.com>
2022-09-19 23:47:02 +0200

    will try with vue, if its too tedious i wont continue with it

--- src/Python/webserver/client/src/Router.js
+++ src/Python/webserver/client/src/Router.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import React from 'react'
 import Login from "./pages/Login"
 import NotFound from "./pages/NotFound"
 import Messenger from "./pages/Messenger"
