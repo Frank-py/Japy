306fc32f0a302683fb9f0ffc481bc7d97675b3a7
wertzuz <quint.schartel@gmail.com>
2022-11-23 21:22:46 +0100

    files

---
+++ src/Java/testuser.java
@@ -0,0 +1,14 @@
+package Java;
+
+public class testuser {
+
+    testuser(String[] UP){
+    String username = UP[0];
+    String pw = UP[1];
+    new messInterface();
+    new encry();
+
+
+    }
+    
+}
