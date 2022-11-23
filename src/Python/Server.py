b70cbe673e5da9410637ba5b18cd49f92f46635a
Daniel <lenj94002@gmail.com>
2022-11-23 21:34:45 +0100

    test

--- src/Python/Server.py
+++ src/Python/Server.py
@@ -46,20 +46,20 @@ async def Client(reader,writer):
         data.append(response)
         if data[0] == "getMes":
             print("getmes")
-            response = (await reader.read(512).decode("utf-8"))
+            response = (await reader.read(512)).decode("utf-8")
             writer.write("200\n".encode("utf-8"))
             await writer.drain()
-            if checkforquit(response, writer):
+            if await checkforquit(response, writer):
                 return
             data.append(response)
             nachrichte = benutzer.checkformessages(data[1])
             writer.write(nachrichte.encode("utf-8")+"\n".encode("utf-8"))
             await writer.drain()
         if data[0] == "proofuser":
-            response = (await reader.read(512).decode("utf-8"))
+            response = (await reader.read(512)).decode("utf-8")
             writer.write("200\n".encode("utf-8"))
             await writer.drain()
-            if checkforquit(response, writer):
+            if await checkforquit(response, writer):
                 data.append(response)
             if benutzer.searchaccount(data[1]):
                 writer.write("1\n".encode('utf-8'))
@@ -69,20 +69,18 @@ async def Client(reader,writer):
                 await writer.drain()
         if data[0] == "sendMes":
             print("Nachrichtanfrage erfolgreich abgeschlossen")
-            response = (await reader.read(512).decode("utf-8"))
+            response = (await reader.read(512)).decode("utf-8")
             writer.write("200\n".encode("utf-8"))
             await writer.drain()
-            if checkforquit(response, writer):
+            if await checkforquit(response, writer):
                 data.append(response)
             data.append(response)
             writer.write("200\n".encode("utf-8"))
             await writer.drain()
-            response = (await reader.read(5000).decode("utf-8"))
+            response = (await reader.read(5000)).decode("utf-8")
             writer.write("200\n".encode("utf-8"))
             await writer.drain()
-            if response == "quit":
-                writer.write("quit\n".encode("utf-8"))
-                await writer.drain()
+            if await checkforquit(response, writer):
                 return
             data.append(response)
             writer.write("200\n".encode("utf-8"))
@@ -92,11 +90,9 @@ async def Client(reader,writer):
             writer.write("200\n".encode("utf-8"))
             await writer.drain()
         if data[0] == "createKey":
-            response = (await reader.read(512).decode("utf-8"))
+            response = (await reader.read(512)).decode("utf-8")
             writer.write("200\n".encode("utf-8"))
-            if response == "quit":
-                writer.write("quit\n".encode("utf-8"))
-                await writer.drain()
+            if await checkforquit(response, writer):
                 return
             data.append(response)
             writer.write("200\n".encode("utf-8"))
