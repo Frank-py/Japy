d9ac61c616e5a4f0cd570464ef82ac19c1440ebd
Daniel <lenj94002@gmail.com>
2022-11-30 18:48:07 +0100

    testasdd

--- src/Python/Server.py
+++ src/Python/Server.py
@@ -3,128 +3,133 @@ from functionsserver import *
 PORT = 6000
 async def checkforquit(text, writer): 
     if text == "quit":
-        writer.write("quit\n".encode("latin"))
+        writer.write("quit\n".encode("utf-8"))
         await writer.drain()
         return True 
 
 async def Client(reader,writer):
     try:
         while True:
-            command_request = (await reader.read(512)).decode("latin")
-            writer.write("200\n".encode("latin"))
+            command_request = (await reader.read(512)).decode("utf-8")
+            writer.write("200\n".encode("utf-8"))
             await writer.drain()
             if await checkforquit(command_request, writer):
                 return
             user_and_password = []
             if command_request == "login":
                 for i in range(2):
-                    response = (await reader.read(512)).decode("latin") # adds user and password to list
-                    writer.write("200\n".encode("latin"))
+                    response = (await reader.read(512)).decode("utf-8") # adds user and password to list
+                    writer.write("200\n".encode("utf-8"))
                     await writer.drain()
                     if await checkforquit(response, writer):
                         return
                     user_and_password.append(response)
-                benutzer = User().checkaccount(user_and_password[0], hashlib.md5(bytes(user_and_password[1], encoding='latin')).hexdigest())
+                benutzer = User().checkaccount(user_and_password[0], hashlib.md5(bytes(user_and_password[1], encoding='utf-8')).hexdigest())
                 if benutzer.loggedin and benutzer.registriert:
-                    writer.write("0\n".encode('latin'))
+                    writer.write("0\n".encode('utf-8'))
                     await writer.drain()
                     break
                 elif benutzer.loggedin:
-                    writer.write("1\n".encode('latin'))
+                    writer.write("1\n".encode('utf-8'))
                     await writer.drain()
                     break
                 else:
-                    writer.write("2\n".encode('latin')) 
+                    writer.write("2\n".encode('utf-8')) 
                     await writer.drain()
         while True: 
-            command_request = (await reader.read(512)).decode("latin")
-            writer.write("200\n".encode("latin"))
+            command_request = (await reader.read(512)).decode("utf-8")
+            writer.write("200\n".encode("utf-8"))
             await writer.drain()
             if await checkforquit(response, writer):
                 return
            
             if command_request == "getMes":
-                user = (await reader.read(512)).decode("latin")
-                writer.write("200\n".encode("latin"))
+                print("getMes")
+                user = (await reader.read(512)).decode("utf-8")
+                writer.write("200\n".encode("utf-8"))
                 await writer.drain()
                 if await checkforquit(user, writer):
                     return
                 messages = benutzer.checkformessages(user)
-                writer.write(messages.encode("latin")+"\n".encode("latin"))
+                print(messages)
+                writer.write((messages+"\n").encode("utf-8"))
                 await writer.drain()
             if command_request == "proofuser":
-                user = (await reader.read(512)).decode("latin")
-                writer.write("200\n".encode("latin"))
+                print("proofuser")
+                user = (await reader.read(512)).decode("utf-8")
+                writer.write("200\n".encode("utf-8"))
                 await writer.drain()
                 if await checkforquit(user, writer):
                     return
 
                 if benutzer.searchaccount(user):
-                    writer.write("1\n".encode('latin'))
+                    writer.write("1\n".encode('utf-8'))
                     await writer.drain()
                 else:
-                    writer.write("0\n".encode('latin'))
+                    writer.write("0\n".encode('utf-8'))
                     await writer.drain()
             if command_request == "sendMes":
-                user = (await reader.read(512)).decode("latin")
-                writer.write("200\n".encode("latin"))
+                print("sendMes")
+                user = (await reader.read(512)).decode("utf-8")
+                writer.write("200\n".encode("utf-8"))
                 await writer.drain()
                 if await checkforquit(user, writer):
                     return
             
-                messages = (await reader.read(5000)).decode("latin")
-                writer.write("200\n".encode("latin"))
+                messages = (await reader.read(5000)).decode("utf-8")
+                writer.write("200\n".encode("utf-8"))
                 await writer.drain()
                 if await checkforquit(messages, writer):
                     return
                 benutzer.insertmessage(user, messages) 
-                writer.write("200\n".encode("latin"))
+                writer.write("200\n".encode("utf-8"))
                 await writer.drain()
-            if data[0] == "createKey":
-                user = (await reader.read(512)).decode("latin") #user
-                writer.write("200\n".encode("latin"))
+            if command_request == "createKey":
+                print("createKey")
+                user = (await reader.read(512)).decode("utf-8") #user
+                writer.write("200\n".encode("utf-8"))
                 if await checkforquit(user, writer):
                     return 
-                status = benutzer.createKey(user).encode("latin")+"\n".encode("latin")
+                status = benutzer.createKey(user).encode("utf-8")+"\n".encode("utf-8")
                 if benutzer.status == 1:
-                    writer.write("1\n".encode("latin"))
+                    writer.write("1\n".encode("utf-8"))
                     await writer.drain()
                     continue
                 elif benutzer.status == 0:
                     writer.write(status)
                     await writer.drain()
-                    P = (await reader.read(512)).decode(encoding="latin")
-                    writer.write("200\n".encode("latin"))
+                    P = (await reader.read(512)).decode(encoding="utf-8")
+                    writer.write("200\n".encode("utf-8"))
                     await writer.drain()
-                    G = (await reader.read(512)).decode(encoding="latin")
-                    writer.write("200\n".encode("latin"))
+                    G = (await reader.read(512)).decode(encoding="utf-8")
+                    writer.write("200\n".encode("utf-8"))
                     await writer.drain()
-                    A = (await reader.read(512)).decode(encoding="latin")
-                    writer.write("200\n".encode("latin"))
+                    A = (await reader.read(512)).decode(encoding="utf-8")
+                    writer.write("200\n".encode("utf-8"))
                     await writer.drain()
-                    benutzer.insertKeys(data[1], P, G, A)
+                    benutzer.insertKeys(user, P, G, A)
                     print(f"{P:}{G:}{A:}")
                 elif benutzer.status == 2:
                     writer.write(status)
                     await writer.drain()
-                    P,G,A = benutzer.getKeys(data[1])
-                    writer.write((P+"\n").encode("latin"))
+                    P,G,A = benutzer.getKeys(user)
+                    writer.write((P+"\n").encode("utf-8"))
                     await writer.drain()
-                    writer.write((G+"\n").encode("latin"))
+                    writer.write((G+"\n").encode("utf-8"))
                     await writer.drain()
-                    writer.write((A+"\n").encode("latin"))
+                    writer.write((A+"\n").encode("utf-8"))
                     await writer.drain()
-                    B = (await reader.read(512)).decode(encoding="latin")
-                    writer.write("200\n".encode("latin"))
+                    B = (await reader.read(512)).decode(encoding="utf-8")
+                    writer.write("200\n".encode("utf-8"))
                     await writer.drain()
-                    benutzer.insertKeys(user=data[1], aorb = B)
+                    benutzer.insertKeys(user=user, aorb = B)
                 elif benutzer.status == 3:
                     writer.write(status)
                     await writer.drain()
-                    P, B = benutzer.getKeys(data[1])
-                    writer.write((P+"\n").encode("latin"))
+                    P, B = benutzer.getKeys(user)
+                    writer.write((P+"\n").encode("utf-8"))
                     await writer.drain()
-                    writer.write((B+"\n").encode("latin"))
+                    writer.write((B+"\n").encode("utf-8"))
                     await writer.drain()
 
     except (ConnectionResetError, BrokenPipeError) as E:
